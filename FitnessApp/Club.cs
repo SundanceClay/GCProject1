3fbdbd426e67713a448fadd05dc1198431de4e3d
lonnie375 <lonnie.e.johnson@gmail.com>
2022-03-21 20:51:54 -0500

    Added 4 Clubs

---
+++ FitnessApp/Club.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp
+{
+    public class Club
+    {
+        public string street { get; set; }
+        public string city { get; set; }
+
+        public string state { get; set; }
+
+        public string postalCode { get; set; }
+        public string clubName { get; set; }
+
+        public string id { get; set; }
+
+
+        public Club(string street, string city, string state, string postalCode, string clubName, string id)
+        {
+            this.street = street;
+            this.city = city;
+            this.state = state;
+            this.postalCode = postalCode;
+            this.clubName = clubName;
+            this.id = id; 
+        }
+    }
+
+  
+
+ 
+}
--- FitnessApp/Program.cs
+++ FitnessApp/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+
+
+//Create a list of 4 fitness center loacations 
+
+//Enter information about the clubs (Street, city, state, postalcode, clubname, id)
+using FitnessApp;
+
+List<Club> clubList = new()
+{
+    new Club("Gratiot", "Detroit", "Michigan", "48224", "Detroit Club", "55555"),
+    new Club("Lansing", "Lansing", "Michigan", "48864", "Lansing Club", "66666"),
+    new Club("Airport Blvd", "Ann Arbor", "Michigan", "48103", "Ann Arbor Club", "77777"),
+    new Club("23 Mile Road", "Rochester Hills", "Michigan", "48307", "Rochester Club", "88888"),
+};
+
+
+
+foreach(var club in clubList)
+{
+    if(club.id == "55555")
+    {
+        Console.WriteLine($" {club.clubName} {club.id} - {club.street} {club.city}, {club.state} {club.postalCode}");
+
+    }
+
+}
