8fdd5713d758ab8103675a1c2ec84d691a27ee80
Lonnie Johnson <lonnie.e.johnson@gmail.com>
2022-03-30 19:47:40 -0500

    Adding Test and Changed Name.

---
+++ FitnessAppTests/SingleClubCheckIn.cs
@@ -0,0 +1,33 @@
+﻿using FitnessApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FitnessAppTests
+{
+    public class SingleClubCheckIn
+    {
+        [Fact]
+
+        public void SingleClubMember()
+        {
+            //Arrange 
+            Club club = new Club(4, "Rochester Club", "23 Mile Road", "Rochester Hills", "Michigan", "48307");
+            SingleClubMember single = new SingleClubMember(1, "TestingName", 1);
+            SingleClubMember single2 = new SingleClubMember(3, "TestingName", 2);
+            //Act
+            single.CheckIn(club);
+            //Assert
+            Assert.True(single.Id == 1); //Confirm that id and club id match. If the club id is 
+            Assert.True(single2.Id == 2);
+            Assert.True(single2.Id != 3);
+
+
+            
+        }
+
+    }
+}
